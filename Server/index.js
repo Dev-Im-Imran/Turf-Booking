337bb663a0b8bc964aabf16b2ed22be8b4d56255
unknown <Dev-Im-Imran>
2026-01-03 13:28:59 +0000

    Fix: converted embedded repo 'turf-booking' to a regular folder

--- Server/index.js
+++ Server/index.js
@@ -1,4 +1,31 @@
-import express from "express";
-import bodyParser from "body-parser";
-import cors from "cors";
-import mongoose from "mongoose";
+import express from 'express';
+import cors from 'cors';
+import mongoose, { now } from 'mongoose';
+import userRouter from './routers/userRouter.js'
+
+
+//middlewares
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+
+//routes 
+app.use("/api", userRouter);
+
+
+
+const PORT = process.env.PORT || 5000;
+const CONNECTOR_URL = 'mongodb+srv://imran_DB_User:DBImranFaa@cluster0.yjrp4id.mongodb.net/LogIn?appName=Cluster0'
+
+
+
+mongoose.connect(CONNECTOR_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log('MongoDB connected')
+        app.listen(PORT, () => {
+            console.log('server is running on port', PORT);
+        })
+    })
+    .catch(err => console.log(err));
+
